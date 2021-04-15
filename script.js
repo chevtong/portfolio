e187edcb9c881fd18a62372c12a5c7abae1bb435
chevtong <chevtongwingyan@gmail.com>
2021-04-15 20:46:46 +0200

    WIP - Work on js for adding animation with scroll

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const links = document.querySelectorAll('.nav-links li');
 hamburger.addEventListener('click', ()=>{
     navLinks.classList.toggle("open");
 })
+
+
+function addAnimation(){
+    
+    const contactAnimation = document.querySelector(".contacts-container ")
+    const nameAnimation = document.querySelector(".name-container")
+
+    const namePosition = nameAnimation.getBoundingClientRect().top
+   const titlePosition = contactAnimation.getBoundingClientRect().top
+
+   const screenPosition = window.innerHeight / 1.4;
+
+
+
+
+
+   if(namePosition==0){console.log('yes')}
+
+    console.log('namePosition: ', namePosition)
+   console.log('screenPosition: ', screenPosition)
+   console.log('window.innerHeight: ', window.innerHeight)
+
+   if (titlePosition < screenPosition){
+    contactAnimation.classList.add('animation')
+   } else{
+    contactAnimation.classList.remove('animation')
+   }
+    
+}
+
+window.addEventListener('scroll',addAnimation )
+
+
