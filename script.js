52038aad818c7af20d68cdff7d5a941a1fcf33b5
chevtong <chevtongwingyan@gmail.com>
2021-04-16 00:36:37 +0200

    Add copy email address function

--- script.js
+++ script.js
@@ -1,68 +1,85 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const navLinksTag = document.querySelectorAll(".nav-links li a");
+const emailBtn = document.querySelector(".email-btn")
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
 
-const navLinksTag = document.querySelectorAll(".nav-links li a")
+
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
+
 
 
 function toggleNav(elem) {
-
-    navLinksTag.forEach(tag=>{
-        tag.classList.remove("nav-active")
-    })
-    elem.classList.add("nav-active")
-    
+  navLinksTag.forEach((tag) => {
+    tag.classList.remove("nav-active");
+  });
+  elem.classList.add("nav-active");
 }
-navLinksTag.forEach(tag=>{
-    tag.addEventListener("click", ()=>{toggleNav(tag)})
-})
-
 
 function addAnimation() {
   const contactAnimation = document.querySelector(".contacts-container");
   const projectAnimation = document.querySelector(".projects-title");
   const nameAnimation = document.querySelector(".name");
 
-  const landingTab = document.querySelector(".landing-tab")
-  const projectTab = document.querySelector(".projects-tab")
-  
-  const contactsTab = document.querySelector(".contacts-tab")
+  const landingTab = document.querySelector(".landing-tab");
+  const projectTab = document.querySelector(".projects-tab");
 
-  const projectPostion =  document.querySelector(".projects-container").getBoundingClientRect().top;
-  const namePosition = document.querySelector(".name-container").getBoundingClientRect().top;
+  const contactsTab = document.querySelector(".contacts-tab");
+
+  const projectPostion = document
+    .querySelector(".projects-container")
+    .getBoundingClientRect().top;
+  const namePosition = document
+    .querySelector(".name-container")
+    .getBoundingClientRect().top;
   const titlePosition = contactAnimation.getBoundingClientRect().top;
 
-  const screenPosition = window.innerHeight/ 1.4;
+  const screenPosition = window.innerHeight / 1.4;
 
   if (namePosition == 0) {
     nameAnimation.classList.add("name-animation");
-    toggleNav(landingTab)
+    toggleNav(landingTab);
   } else {
     nameAnimation.classList.remove("name-animation");
   }
   if (projectPostion < screenPosition) {
     projectAnimation.classList.add("animation");
-    toggleNav(projectTab)
+    toggleNav(projectTab);
   } else {
     projectAnimation.classList.remove("animation");
   }
   if (titlePosition < screenPosition) {
     contactAnimation.classList.add("animation");
-    toggleNav(contactsTab)
+    toggleNav(contactsTab);
   } else {
     contactAnimation.classList.remove("animation");
   }
 
-  console.log('titlePosition', titlePosition)
-  console.log('screenPosition', screenPosition)
 }
 
+navLinksTag.forEach((tag) => {
+  tag.addEventListener("click", () => {
+    toggleNav(tag);
+  });
+});
 
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
 
-window.addEventListener('scroll', ()=>{
-    addAnimation()
-}, 3000)
+window.addEventListener(
+  "scroll",
+  () => {
+    addAnimation();
+  },
+  3000
+);
