25dd28aa4d2940a10240922337126a24e3063e59
chevtong <chevtongwingyan@gmail.com>
2021-04-18 01:06:23 +0200

    Finish the project sections

--- script.js
+++ script.js
@@ -3,6 +3,38 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const navLinksTag = document.querySelectorAll(".nav-links li a");
 const emailBtn = document.querySelector(".email-btn");
+const mainImg = document.querySelector(".project-img-main")
+const thumbnailsImg = document.querySelectorAll(".project-img-thumbnail")
+const mainImg2 = document.querySelector(".project-img-main2")
+const thumbnailsImg2 = document.querySelectorAll(".project-img-thumbnail2")
+const mainImg1 = document.querySelector(".project-img-main1")
+const thumbnailsImg1 = document.querySelectorAll(".project-img-thumbnail1")
+
+
+
+
+thumbnailsImg.forEach(thumbnail=>{
+
+  thumbnail.addEventListener("click", ()=>{
+   
+     mainImg.src=thumbnail.src
+  })
+})
+thumbnailsImg1.forEach(thumbnail=>{
+
+  thumbnail.addEventListener("click", ()=>{
+   
+     mainImg1.src=thumbnail.src
+  })
+})
+thumbnailsImg2.forEach(thumbnail=>{
+  thumbnail.addEventListener("click", ()=>{
+    
+     mainImg2.src=thumbnail.src
+  })
+})
+
+
 
 function CopyToClipboard(id) {
   var r = document.createRange();
@@ -13,21 +45,16 @@ function CopyToClipboard(id) {
   window.getSelection().removeAllRanges();
 }
 
-function toggleNav(elem) {
-  navLinksTag.forEach((tag) => {
-    tag.classList.remove("nav-active");
-  });
-  elem.classList.add("nav-active");
-}
+
 
 function addAnimation() {
   const contactAnimation = document.querySelector(".contacts-container");
   const projectAnimation = document.querySelector(".projects-title");
+  const profileAnimation = document.querySelector(".profile-title");
   const nameAnimation = document.querySelector(".name");
 
   const landingTab = document.querySelector(".landing-tab");
   const projectTab = document.querySelector(".projects-tab");
-
   const contactsTab = document.querySelector(".contacts-tab");
 
   const projectPostion = document
@@ -37,6 +64,7 @@ function addAnimation() {
     .querySelector(".name-container")
     .getBoundingClientRect().top;
   const titlePosition = contactAnimation.getBoundingClientRect().top;
+  const profilePosition = profileAnimation.getBoundingClientRect().top;
 
   const screenPosition = window.innerHeight / 1.4;
 
@@ -45,6 +73,12 @@ function addAnimation() {
     toggleNav(landingTab);
   } else {
     nameAnimation.classList.remove("name-animation");
+  }
+  if (profilePosition < screenPosition) {
+    profileAnimation.classList.add("animation");
+    toggleNav(projectTab);
+  } else {
+    profileAnimation.classList.remove("animation");
   }
   if (projectPostion < screenPosition) {
     projectAnimation.classList.add("animation");
@@ -61,6 +95,12 @@ function addAnimation() {
 }
 
 
+function toggleNav(elem) {
+  navLinksTag.forEach((tag) => {
+    tag.classList.remove("nav-active");
+  });
+  elem.classList.add("nav-active");
+}
 
 navLinksTag.forEach((tag) => {
   tag.addEventListener("click", () => {
@@ -70,6 +110,9 @@ navLinksTag.forEach((tag) => {
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
+  links.forEach(link=>link.addEventListener('click', ()=>{
+    navLinks.classList.remove("open");
+  }))
 });
 
 window.addEventListener("scroll", addAnimation);
